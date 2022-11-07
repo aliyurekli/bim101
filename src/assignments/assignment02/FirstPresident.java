e1ca32edea3930ac696c86a8cd48f1699bc2d071
Ali Yürekli <yurekli.ali@gmail.com>
2022-11-07 15:54:27 +0200

    Solution for Assignment 2 is added

--- src/assignments/assignment02/FirstPresident.java
+++ src/assignments/assignment02/FirstPresident.java
@@ -27,7 +27,31 @@ public class FirstPresident {
     public static void main(String[] args) {
         int year = getRandomYear();
 
-        // Write your code here below
-
+        switch (year) {
+            case 1881:
+                System.out.printf("In %d, Mustafa was born in Salonika.", year);
+                break;
+            case 1893:
+                System.out.printf("In %d, Mustafa was given the additional name Kemal at military school.", year);
+                break;
+            case 1919:
+                System.out.printf("In %d, Mustafa Kemal landed in Samsun.", year);
+                break;
+            case 1923:
+                System.out.printf("In %d, Mustafa Kemal was elected as the first president of the Republic of Turkey.", year);
+                break;
+            case 1932:
+                System.out.printf("In %d, Mustafa Kemal founded the Turkish Linguistic Society.", year);
+                break;
+            case 1934:
+                System.out.printf("In %d, the Grand National Assembly of Turkey granted him the surname Atatürk.", year);
+                break;
+            case 1938:
+                System.out.printf("In %d, Atatürk died in Istanbul.", year);
+                break;
+            default:
+                System.out.printf("One of his favorite sayings is \"%s\"", getRandomQuote());
+                break;
+        }
     }
 }
