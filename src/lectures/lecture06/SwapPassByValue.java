5c3eb01770eb1e247f2d3fbacb4fd3aea2a829c8
Ali Yürekli <yurekli.ali@gmail.com>
2022-11-22 22:23:38 +0300

    Sample codes of Lecture 6 are added

---
+++ src/lectures/lecture06/SwapPassByValue.java
@@ -0,0 +1,25 @@
+package lectures.lecture06;
+
+public class SwapPassByValue {
+
+    public static void swap(int n1, int n2) {
+        System.out.println("Before swapping, n1 is " + n1 + " and n2 is " + n2);
+
+        int temp = n1;
+        n1 = n2;
+        n2 = temp;
+
+        System.out.println("After swapping, n1 is " + n1 + " and n2 is " + n2);
+    }
+
+    public static void main(String[] args) {
+        int num1 = 1;
+        int num2 = 2;
+
+        System.out.println("Before invoking swap method, num1 is " + num1 + " and num2 is " + num2);
+
+        swap(num1, num2);
+
+        System.out.println("After invoking swap method, num1 is " + num1 + " and num2 is " + num2);
+    }
+}
