daaaeae53e6caa7cb1a12d35bec96419a268a692
Ali Yürekli <yurekli.ali@gmail.com>
2022-12-28 18:02:54 +0300

    Sample code for UML design of Assignment 6 is added

---
+++ src/assignments/assignment06/CengStudent.java
@@ -0,0 +1,61 @@
+package assignments.assignment06;
+
+import java.time.LocalDate;
+
+/**
+ * Please refer to the material in Assignment #6
+ */
+public class CengStudent {
+
+    private static final String DEPARTMENT = "Computer Engineering";
+
+    private final String id;
+
+    private final String fullName;
+
+    private LocalDate birthDate;
+
+    private int semester;
+
+    public CengStudent(String id, String fullName) {
+        this.id = id;
+        this.fullName = fullName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public int getSemester() {
+        return semester;
+    }
+
+    public void setSemester(int semester) {
+        this.semester = semester;
+    }
+
+    public boolean isBirthday() {
+        if (birthDate != null) {
+            LocalDate now = LocalDate.now();
+            return now.getMonth() == birthDate.getMonth() && now.getDayOfMonth() == birthDate.getDayOfMonth();
+        }
+        else
+            return false;
+    }
+
+    static String getDepartment() {
+        return DEPARTMENT;
+    }
+}
